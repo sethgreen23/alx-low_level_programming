74a0e7719ef767815f9c362c394f3084c8b8718d
sethgreen23 <dridi.chaith@gmail.com>
2022-09-08 12:39:22 -0700

    lskjdsjs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  * Return: print a string to the starndard error
  */
 int main(void)
-{
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+{i
+	int s;
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);	
 	return (1);
 }
