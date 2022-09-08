ee7b5bbce5efdb474fb156646643a5a4c52e32b5
sethgreen23 <dridi.chaith@gmail.com>
2022-09-08 11:02:26 -0700

    stdout

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+ * main - main function
+ *
+ * Return: print a string to the starndard error
+ */
+int main(void)
+{
+	int str_len = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",str_len);
+	return (1);
+}
