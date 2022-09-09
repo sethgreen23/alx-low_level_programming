33e33bdf3ba8783764e37220ca885259978c8e19
sethgreen23 <dridi.chaith@gmail.com>
2022-09-09 09:32:00 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: print all the alphabet in 
+ * lower case each one followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 97;
+
+	while(letter < 123)
+	{
+
+		printf("%c", letter++);
+
+	}
+
+	printf("\n");
+
+	return (0);
+}
