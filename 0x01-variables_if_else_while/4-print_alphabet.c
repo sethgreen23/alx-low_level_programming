d4834f90ff6a9348fccefcd14272bbfb1e184f7a
sethgreen23 <dridi.chaith@gmail.com>
2022-09-09 10:05:45 -0700

    without e q

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: print all the alphabet in lower case
+ * each one followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 97;
+
+	while (letter < 123)
+	{
+		
+		if (letter == 'e' || letter== 'q')
+
+			continue;
+
+			printf("%c", letter++);
+
+	}
+
+	printf("\n");
+
+	return (0);
+}
