2a91c1a629ea9f9b45a9e38e8b88d075f3edcb65
sethgreen23 <dridi.chaith@gmail.com>
2022-09-09 10:47:31 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Description: print all number from 1 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 1;
+
+	while (number < 10)
+	{
+
+		printf("%d", number++);
+
+	}
+
+	printf("\n");
+
+	return (0);
+}
