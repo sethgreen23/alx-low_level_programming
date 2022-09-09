4d759aa01b1d78b420451a3a19a78b9d7abe4202
sethgreen23 <dridi.chaith@gmail.com>
2022-09-09 12:45:25 -0700

    printing all the single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Description: printing all single digit
+ * sparated by ',' and ' '
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int character;
+
+	character = 0;
+
+	while (character < 10)
+	{
+
+		putchar(character + '0');
+
+		if (character < 9)
+		{
+
+			putchar(',');
+			putchar(' ');
+
+		}
+
+		character++;
+	}
+
+	return (0);
+}
