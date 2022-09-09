68a86cecd2567a06548e88f657df1b1b4720f8d5
sethgreen23 <dridi.chaith@gmail.com>
2022-09-09 11:47:59 -0700

    reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Description: printing the alphabet
+ * in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	char character;
+
+	character = 'z';
+
+	while (character >= 'a')
+	{
+	
+		putchar(character--);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
