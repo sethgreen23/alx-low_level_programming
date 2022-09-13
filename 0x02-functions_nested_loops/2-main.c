122aa9e21e771db11c8965f3be29a56f5a00e513
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 11:15:47 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - main function
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int counter;
+
+	print_alphabet_x10();
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - secondary function
+ *
+ * Description: print the alphabet 10 times
+ *
+ * Return: return nothing
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 10 ; i++)
+	{
+		print_aphabet();
+	}
+	putchar('\n');
+}
