d53a471baa78f917ec30340cf90b0e78a9baca96
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 06:18:56 -0700

    putchar altered

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,30 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 #include<string.h>
+#include "main.h"
+
+/**
+ * print_putchar - secondary function
+ *
+ * Description: print the string "_putchar"
+ *
+ * Return: return nothing
+ */
+
+void print_putchar(void)
+{
+	printf("_putchar\n");
+}
+
 /**
  * main - main function
- * 
+ *
  * Description: print the string _putchar
- * 
+ *
  * Return: Always 0 (Success)
  */
-int main(void) {
-    char* str="_putchar";
-    
-    printf("%s\n", str);
-    return 0;
+
+int main(void)
+{
+print_putchar();
+return	(0);
 }
