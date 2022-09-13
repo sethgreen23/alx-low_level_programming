bd778749399bf6d1e846ae4f56be66976a49ac5b
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 12:01:13 -0700

    lksdmjfsl

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _islower - check integer intered is lowercase of not
+ *
+ * @alpha: is the integer we will check
+ *
+ * Return: return 1 if lowercase and 1 is not
+ */
+
 int _islower(int alpha)
 {
-	if(alpha >= 97 && alpha <= 122)
+	if (alpha >= 97 && alpha <= 122)
 		return (1);
 	return (0);
 }
