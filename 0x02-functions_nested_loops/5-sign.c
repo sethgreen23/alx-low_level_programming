3fa7a4542104ef2fdabe5988e393aa743bd96fbb
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 12:34:17 -0700

    print the sight

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - print the sign of an integer
+ *
+ * @n: the integer to print the sign of
+ *
+ * Return: 0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+}
