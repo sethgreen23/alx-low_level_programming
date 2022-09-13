d8edde97548a7006202366231019def9d15f32df
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 05:50:43 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+/**
+ * main - main function
+ * 
+ * Description: print the string _putchar
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+    char* str="_putchar";
+    
+    printf("%s\n", str);
+    return 0;
+}
