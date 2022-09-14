87c067546db8251f80daea73a0416a032b951e89
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 17:28:17 -0700

    mlqskjm

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,12 @@ void times_table(void)
 	{
 		for (y = 0; y < 10; y++)
 		{
+		if (y == 0)
+			printf("%d,", x * y);
 		if (y < 9)
-			printf("%3d,", x*y);
+			printf("%3d,", x * y);
 		else
-			printf("%3d", x*y);
+			printf("%3d", x * y);
 		}
 		printf("\n");
 	}
