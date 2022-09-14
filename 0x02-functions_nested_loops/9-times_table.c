278c01c2dc9d46a0dcd7557127ec37d82d141601
sethgreen23 <dridi.chaith@gmail.com>
2022-09-13 17:22:34 -0700

    9 multiprlication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - print 9 times multipation times_table
+ *
+ * Return: return nothing
+ */
+
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+		if (y < 9)
+			printf("%3d,", x*y);
+		else
+			printf("%3d", x*y);
+		}
+		printf("\n");
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int  print_sign(int n);
 int _abs(int i);
 int print_last_digit(int number);
 void jack_bauer(void);
+void times_table(void);
 #endif
