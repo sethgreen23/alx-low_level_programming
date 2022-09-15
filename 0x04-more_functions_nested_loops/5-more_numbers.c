651731a400f57e6033fdc2fb9ea52f379993ff11
sethgreen23 <dridi.chaith@gmail.com>
2022-09-15 07:50:12 -0700

    lksjlsj

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,17 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, tens, digit;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			tens = j / 10;
+			digit = j % 10;
+			if (tens == 1)
+				_puthchar(tens + '0');
+			_putchar(digit + '0');
 		}
 		_putchar('\n');
 	}
