ec809734b9e8a8321baac406cdffb8aab78c107b
sethgreen23 <dridi.chaith@gmail.com>
2022-09-15 05:53:06 -0700

    multiply two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply two digit
+ *
+ * @a: first digit of the operation
+ *
+ * @b: second digit of the operation
+ *
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 function and nested loops
 0-isupper: check if character upper case
 1-isdigit: check if digit
+2-mul: multiply two digits
