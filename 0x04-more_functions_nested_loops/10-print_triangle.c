82d833916620d41850a7db47d023888a75797bc4
sethgreen23 <dridi.chaith@gmail.com>
2022-09-15 13:20:49 -0700

    lskjdlsjfljsm

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,21 +3,21 @@
 /**
  * print_triangle - print triable
  *
- * @n: size of the triagle
+ * @size: size of the triagle
  *
  * Return: nothing
  */
-void print_triangle(int n)
+void print_triangle(int size)
 {
 	int i, j;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
-				if (j < n - i)
+				if (j < (size - i))
 					printf(" ");
 				else
 					printf("#");
