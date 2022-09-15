4042ca801be9d33e5cffab5db156c801c333adae
sethgreen23 <dridi.chaith@gmail.com>
2022-09-15 09:12:36 -0700

    lkjlskjdljslks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,7 +15,7 @@ void print_diagonal(int n)
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
-				_putchar('\t');
+				_putchar(' ');
 			_putchar('\\');
 			if (i != (n - 1))
 				_putchar('\n');
