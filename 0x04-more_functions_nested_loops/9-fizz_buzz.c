8cee01b3b0f8e4b1138544ce187cf906c257c318
sethgreen23 <dridi.chaith@gmail.com>
2022-09-15 12:38:21 -0700

    lkjslkjfds

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,29 +5,29 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 
-for (i = 1; i <= 100; i++)
-{
-	if (((i % 3) == 0) && ((i % 5) == 0))
+	for (i = 1; i <= 100; i++)
 	{
-		printf("FizzBuzz");
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			printf(" ");
 	}
-	else if ((i % 3) == 0)
-	{
-		printf("Fizz");
-	}
-	else if ((i % 5) == 0)
-	{
-		printf("Buzz");
-	}
-	else
-	{
-		printf("%d", i);
-	}
-	if (i != 100)
-		printf(" ");
-}
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
