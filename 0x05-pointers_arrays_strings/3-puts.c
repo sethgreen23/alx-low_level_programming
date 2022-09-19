3caac7d0b367588652f55a5def34c3318ece61eb
sethgreen23 <dridi.chaith@gmail.com>
2022-09-19 05:27:06 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _puts - print string
+ *
+ * @str: str to print
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, '\n', 1);
+}
