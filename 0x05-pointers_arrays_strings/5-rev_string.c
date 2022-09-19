7ebd485d8f236e31e9121c5106978e49348bba34
sethgreen23 <dridi.chaith@gmail.com>
2022-09-19 08:54:43 -0700

    lksjlsjfmslkjdm

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ void rev_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	l = i;
-	for (i--, c = 0; j < l / 2; i--, j++)
+	for (i--, j = 0; j < l / 2; i--, j++)
 	{
 		c = str[i];
 		str[i] = str[j];
