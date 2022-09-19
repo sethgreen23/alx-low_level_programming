c656b9a678e379fa98e7111b1b9d3668b1b646f3
sethgreen23 <dridi.chaith@gmail.com>
2022-09-19 05:12:21 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * _strlen - count the length of string
+ *
+ * @s: the string
+ *
+ * Return: return the length of the string
+ */
+int _strlen(char *s)
+{
+	int count = 1;
+
+	while (*s != '\0')
+	{
+		counter++;
+	}
+
+	return (counter);
+}
