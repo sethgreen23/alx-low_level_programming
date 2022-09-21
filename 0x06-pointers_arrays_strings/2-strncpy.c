eae2b06df6531f7ce2797a17565427bfcbcfd0fa
sethgreen23 <dridi.chaith@gmail.com>
2022-09-21 13:30:38 -0700

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.c"
+/**
+ * _strncpy - copy a string
+ *
+ * @dest: destination
+ *
+ * @src: source string
+ *
+ * @n: number of element to copy
+ *
+ * Return: return string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i  < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
