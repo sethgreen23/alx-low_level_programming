0a0f9ddbdb9c196c58142bac3f23ee9db75878b2
sethgreen23 <dridi.chaith@gmail.com>
2022-09-26 19:35:31 -0700

    sum of diagonars

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagsums - calculate the sum of diagonals
+ *
+ * @a: table
+ *
+ * @size: size of the table
+ *
+ * Return: nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0, j, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			if (i == j)
+				sum1 += *(a + j + size + i);
+			if ((i + j) == (size - 1))
+				sum2 += *(a + j + size + i);
+	}
+	printf("%d, %d\n", sum1, sum2);
+
+}
