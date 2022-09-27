72b9619575bdd788540a22c436a092d256b335e6
sethgreen23 <dridi.chaith@gmail.com>
2022-09-26 19:41:46 -0700

    lskjdmsj

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,16 +10,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0, j, sum1 = 0, sum2 = 0;
+	int i, j, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				sum1 += *(a + j + size + i);
+				sum1 += *(a + j + size * i);
 			if ((i + j) == (size - 1))
-				sum2 += *(a + j + size + i);
+				sum2 += *(a + j + size * i);
 		}
 	}
 	printf("%d, %d\n", sum1, sum2);
