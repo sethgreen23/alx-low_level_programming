d61bd93d5a657617c34d27dcc3271fda2bfa062b
sethgreen23 <dridi.chaith@gmail.com>
2022-09-28 17:59:16 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <math.h>
+/**
+ * is_prime_number - check the number is prime or not
+ *
+ * @n: the number to check
+ *
+ * Return: number is prime or not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	int sqt = ceil(sqrt(n));
+
+	return (checkPrime(n, sqt, 2));
+}
+/**
+ * checkPrime - check if number is prime or not
+ *
+ * @n: number to check
+ *
+ * @sqt: sqrt of the number n
+ *
+ * @count: counter
+ *
+ * Return: return the number
+ */
+int checkPrime(int n, int sqt, int count)
+{
+	if (count > sqt)
+		return (1);
+	if (n % count != 0)
+		return (checkPrime(n, sqt, count + 1));
+	return (0);
+}
