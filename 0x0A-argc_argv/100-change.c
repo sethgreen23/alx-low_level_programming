c674f81eddb9dac8c75727aa8172321d053ed3aa
sethgreen23 <dridi.chaith@gmail.com>
2022-09-30 10:41:32 -0700

    count the change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - minimal number of coins to change
+ *
+ * @argc: argument counter
+ *
+ * @argv: argument vector
+ *
+ * Return: 1 is error and 0 if not
+ */
+int main(int argc, char *argv[])
+{
+	int coins[] = {25, 10, 2, 1};
+
+	if (argc > 1)
+	{
+		int number;
+
+		number = atoi(argv[1]);
+		if (number < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+		int length;
+
+		length = sizeof(coins) / sizof(int);
+
+		int count, i;
+
+		count	= 0;
+		for (i = 0; i < length; i++)
+		{
+			if (number / coins[i] == 0)
+				continue;
+			count += number / coins[i];
+			number %= coins[i];
+		}
+		printf("%d\n", count);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+
+}
