38afa58f525aadabe9a21a261f3fed3cf3f915ac
sethgreen23 <dridi.chaith@gmail.com>
2022-09-30 08:28:12 -0700

    print the argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print the number of arguments
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	(void) argv;
+	printf("%d", argc);
+	return (0);
+}
