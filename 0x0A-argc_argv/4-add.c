567f711ba68bb25621016f2c9de93c2d9290eec6
sethgreen23 <dridi.chaith@gmail.com>
2022-09-30 09:28:56 -0700

    lskjdmsjdms

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * check_number - check if the string is a number
+ *
+ * @str: string to check
+ *
+ * Return: 0 is not and 1 if is a number
+ */
+int check_number(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i])
+				return (0);
+	}
+	return (1);
+}
+/**
+ * main - add positives numbers
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count, i;
+
+	count = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (check_number(argv[i]))
+		{
+			count += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
