4d41728cc75ba24859298b2de3ee9179a5fc8552
sethgreen23 <dridi.chaith@gmail.com>
2022-10-03 13:07:19 -0700

    lsjlsjmsjms

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	char *tab = malloc(sizeof(char) * size);
-	
+	char *tab;
+
+	*tab = malloc(sizeof(char) * size);
 	if (tab == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
