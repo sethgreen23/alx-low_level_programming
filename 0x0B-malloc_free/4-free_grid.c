3af60a5816c057ae62858231e979b98fb67c6137
sethgreen23 <dridi.chaith@gmail.com>
2022-10-04 16:16:08 -0700

    free the hole thing

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * free_grid - free the grid
+ *
+ * @grid: it the grid we want to free
+ *
+ * @height: the height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+}
