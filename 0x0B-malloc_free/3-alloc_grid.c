c4bfff05c3db421049a4a72466fd5e115d79be26
sethgreen23 <dridi.chaith@gmail.com>
2022-10-03 17:36:34 -0700

    lksjmslkjsmlkj

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	tab = malloc(sizeof(int) * height);
+	tab = malloc(sizeof(int *) * height);
 	if (tab == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
