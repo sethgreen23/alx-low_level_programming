ec14eb88266f89004ab71c273dcd342d9d77f017
sethgreen23 <dridi.chaith@gmail.com>
2022-10-06 14:20:07 -0700

    mlskjsmlkjsm

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * malloc_checked - malloc a memeory
- *
- * @b: the amount of memeory you allocate
- *
- * Return: the memory allocated
+ * malloc_checked - allocate memory using malloc.
+ * Description: If malloc fails, terminate process with status 98.
+ * @b: unsigned int memory size to allocate
+ * Return: void pointer to malloc'd memory space
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *p;
@@ -13,5 +13,6 @@ void *malloc_checked(unsigned int b)
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
+
 	return (p);
 }
