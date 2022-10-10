06dbb6ca088bdb53bf5d8eaaefa13fd7c81477ae
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 13:21:55 -0700

    mlkjmlsjmlksj

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,54 +1,49 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * new_dog - create a new dog
+ * new_dog - creates new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * Return: dog
+ * Return: a dog object
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *o;
-	char *n;
-	int i, lenn, leno;
+	int i, len1, len2;
+	dog_t *ndog;
+	char *a, *b;
 
-	lenn = 0;
+	len1 = 0;
 	for (i = 0; name[i] != '\0'; i++)
-		lenn++;
-	n = malloc(sizeof(char) * (lenn + 1));
-	if (n == NULL)
-	{
-		free(n);
-		return (NULL);
-	}
-	leno = 0;
+		len1++;
+	len2 = 0;
 	for (i = 0; owner[i] != '\0'; i++)
-		leno++;
-	o = malloc(sizeof(char) * (leno + 1));
-	if (o == NULL)
+		len2++;
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	a = malloc((len1 + 1) * sizeof(char));
+	if (a == NULL)
 	{
-		free(o);
-		free(n);
+		free(ndog);
 		return (NULL);
 	}
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
+	for (i = 0; i < len1; i++)
+		a[i] = name[i];
+	a[i] = '\0';
+	ndog->name = a;
+	ndog->age = age;
+	b = malloc((len2 + 1) * sizeof(char));
+	if (b == NULL)
 	{
-		free(n);
-		free(o);
-		free(d);
+		free(a);
+		free(ndog);
 		return (NULL);
 	}
-	for (i = 0; i < lenn; i++)
-		n[i] = name[i];
-	n[i] = '\0';
-	for (i = 0; i < leno; i++)
-		o[i] = owner[i];
-	o[i] = '\0';
-	d->name = n;
-	d->age = age;
-	d->owner = o;
-	return (d);
+	for (i = 0; i < len2; i++)
+		b[i] = owner[i];
+	b[i] = '\0';
+	ndog->owner = b;
+	return (ndog);
 }
