6ff001e7065092318c4864e0b9473f9b4af5167c
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 12:05:38 -0700

    mlkjsmlskjmsl

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "dog.h"
+/**
+ * new_dog - create a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	d_t *dog;
+
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
