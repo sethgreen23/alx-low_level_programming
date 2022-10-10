8a48a264490ceaf76a99e15f185d3cef65893d5b
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 12:10:06 -0700

    lkjmlskjmslkj

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	d_t *dog;
+	char *o;
+	char *n;
 
+	n = malloc(strlen(name) + 1);
+	if (n == NULL)
+		return (NULL);
+	o = malloc(strlen(owner) + 1);
+	if (o == NULL)
+		return (NULL);
 	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 		return (NULL);
