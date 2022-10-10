9420c8eaf794c22f6c7ad3089ff8589d8c440e72
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 13:15:22 -0700

    mkjsmlsjmslk

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,7 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(n);
 		return (NULL);
 	}
-	d = malloc(sizeof(struct dog *));
+	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 	{
 		free(n);
