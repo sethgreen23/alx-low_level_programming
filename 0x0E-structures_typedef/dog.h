19d898579c97c8d1b11f74cd21c30141a6981cad
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 10:37:39 -0700

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: none
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
