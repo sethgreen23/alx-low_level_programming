4383d16f5b23f19854930f415f490c9e7424f42d
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 18:31:45 -0700

    mlkjmlkjm

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - free the dog
+ * @d: is the dog
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
