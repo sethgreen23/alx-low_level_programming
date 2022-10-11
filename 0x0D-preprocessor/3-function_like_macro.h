64d8342b9b0bc88c167fa6e2cd5dc6418af75a29
sethgreen23 <dridi.chaith@gmail.com>
2022-10-10 17:50:06 -0700

    lkjmlkjmljm

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
-#include <limits.h>
-#define ABS(x) ((x) == (INT_MIN) ? (INT_MAX) : (x) < (0) ? (-x) : (x) )
+#define ABS(x) ((x) < (0) ? (-x) : (x))
 #endif
-
