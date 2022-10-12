f355ae04e3b30bbb61649ba1a7dea05b347ab7e5
sethgreen23 <dridi.chaith@gmail.com>
2022-10-12 11:31:46 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * print_name - print the name
+ * @name: the name to print
+ * @(*f): function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
