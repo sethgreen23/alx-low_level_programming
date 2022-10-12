92d9f2ac048fd1236436df2ee74e3291e20ee077
sethgreen23 <dridi.chaith@gmail.com>
2022-10-12 13:30:59 -0700

    lkjlkjmkjm

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer
+ * @array: array to search in
+ * @size: the size of the array
+ * @cmp: the comapare funtion
+ * Return: return if the array exist or not
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL)
+		exit(98);
+	if (size <= 0)
+		return (-1);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
