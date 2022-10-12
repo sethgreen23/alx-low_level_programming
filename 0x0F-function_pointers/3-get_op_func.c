3b8ef5d7224a5816cbb28bf4f78a56b680e06597
sethgreen23 <dridi.chaith@gmail.com>
2022-10-12 15:46:33 -0700

    mlkjmlkjmlkjmlk

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,9 +6,15 @@
  * @s: the operation
  * Return: the operation related to the function
  */
-int (*get_op_func(char *s))(int, int) {
-	op_t ops[] = {{"+", op_add}, {"-", op_sub},
-		{"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+
 	int i = 0;
 
 	while (ops[i].f != NULL)
