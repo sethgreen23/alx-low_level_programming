8b49957bce5eb7f9c861c4620794d598b0355e7e
sethgreen23 <dridi.chaith@gmail.com>
2022-10-12 11:37:19 -0700

    lkjmjmlj

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,8 +3,10 @@
  * print_name - print the name
  * @name: the name to print
  * @(*f): function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f != NULL)
 		f(name);
 }
