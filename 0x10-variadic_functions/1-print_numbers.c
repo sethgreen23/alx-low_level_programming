429c7b2afbdc316cb555cab0f3ec9d63fa303895
sethgreen23 <dridi.chaith@gmail.com>
2022-10-13 14:20:09 -0700

    mlkjmljmlkj

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,6 +21,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	printf("%d", va_arg(ap, int));
 	for (i = 1; i < n; i++)
 		printf("%s%d", separator, va_arg(ap, int));
-	printf("\n");
 	va_end(ap);
+	printf("\n");
 }
