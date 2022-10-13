1e3e9ac3bbc22299ac560d26c8aba6b317e1ba9d
sethgreen23 <dridi.chaith@gmail.com>
2022-10-13 15:22:43 -0700

    mlkjmlkjmlkj

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - sum of all paramethers
+ * @n: number of element s
+ * @separator: the separator
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	char *check;
+	unsigned int i;
+
+	if (n > 0)
+	{
+	va_start(ap, n);
+	if (separator == NULL)
+		separator = "";
+	check = va_arg(ap, char *);
+	if (check != NULL)
+		printf("%s", check);
+	else
+		printf("(nil)");
+	for (i = 1; i < n; i++)
+	{
+		check = va_arg(ap, char *);
+		if (check != NULL)
+			printf("%s%s", separator, check);
+		else
+		printf("(nil)");
+	}
+	va_end(ap);
+	}
+	printf("\n");
+}
