a382b39b3436742837d6c99039383b6d71146ac9
sethgreen23 <dridi.chaith@gmail.com>
2022-10-13 19:28:50 -0700

    lkjmljmljmk

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,43 +1,91 @@
-#include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
-#include "stdarg.h"
+#include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_all - print all elemnents
- * @format: format all the things
- * Return: nothing
+ * print_c - prints char
+ * @a: list to give
+ * Return: always 0
+ */
+int print_c(va_list a)
+{
+	printf("%c", va_arg(a, int));
+	return (0);
+}
+/**
+ * print_i - prints int
+ * @a: list to give
+ * Return: always 0
+ */
+int print_i(va_list a)
+{
+	printf("%d", va_arg(a, int));
+	return (0);
+}
+/**
+ * print_f - prints float
+ * @a: list to give
+ * Return: always 0
+ */
+int print_f(va_list a)
+{
+	printf("%f", va_arg(a, double));
+	return (0);
+}
+/**
+ * print_s - prints string
+ * @a: list to give
+ * Return: always 0
+ */
+int print_s(va_list a)
+{
+	char *s;
+
+	s = va_arg(a, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return (0);
+	}
+	printf("%s", s);
+	return (0);
+}
+/**
+ * print_all - prints all
+ * @format: format string that says arg types
+ *
  */
 void print_all(const char * const format, ...)
 {
-	va_list argz;
-	char *check;
+	int i, j;
 	char *sep = "";
-	int i = 0;
+	char *sep2 = ", ";
+	va_list anyArgs;
+	printer ops[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"s", print_s},
+		{"f", print_f},
+		{NULL, NULL}
+	};
 
-	va_start(argz, format);
-	while (format && format[i])
+	va_start(anyArgs, format);
+	i = 0;
+	while (format != NULL && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (ops[j].f != NULL)
 		{
-		case 'c':
-			printf("%s%c", sep, va_arg(argz, int));
-		break;
-		case 'i':
-			printf("%s%d", sep, va_arg(argz, int));
-		break;
-		case 'f':
-			printf("%s%f", sep, va_arg(argz, double));
-		break;
-		case 's':
-		check = va_arg(argz, char*);
-		if (check == NULL)
-			check = "(nil)";
-		printf("%s%s", sep, check);
-		break;
+			if (format[i] == *(ops[j].c))
+			{
+				printf("%s", sep);
+				ops[j].f(anyArgs);
+			}
+			j++;
 		}
-	    sep = ", ";
-	    i++;
+		sep = sep2;
+		i++;
 	}
-	va_end(argz);
 	printf("\n");
+	va_end(anyArgs);
 }
