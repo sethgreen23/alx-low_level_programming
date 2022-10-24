9d00331d944c1eac5370817e40f664510f3b6989
sethgreen23 <dridi.chaith@gmail.com>
2022-10-24 14:31:30 -0700

    free with null head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - free the list 
+ * @head: the head of the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	while (*head != NULL)
+	{
+		next = *head;
+		free(*head);
+		*head = next;
+	}
+	*head = NULL;
+}
