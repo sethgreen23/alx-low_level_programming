843d51773099c2f30473b5d3e2c15abf91a4ca5e
sethgreen23 <dridi.chaith@gmail.com>
2022-10-24 11:10:12 -0700

    print the length of the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - print th length of the linkedlist
+ * @h: the head of the list
+ * Return: the length of the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+	listint_t *p;
+
+	count = 0;
+	p = h;
+	if (p != NULL)
+	{
+		while (p != NULL)
+		{
+			count++;
+			p = p->next;
+		}
+	}
+	return (count);
+}
