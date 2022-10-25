f7bf26b1e950d55d969ccd33de7f15169ae8ea60
sethgreen23 <dridi.chaith@gmail.com>
2022-10-24 18:18:44 -0700

    sum of the elements of the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * sum_listint - sum the data of the list
+ * @head: head of the list
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t next;
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	sum = 0;
+	next = head;
+	while (next != NULL)
+	{
+		sum += next->n;
+		next = next->next;
+	}
+	return (sum);
+}
