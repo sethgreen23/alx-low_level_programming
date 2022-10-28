3c1dc54f0438450f4ce2180a38be7a50393ed14d
sethgreen23 <dridi.chaith@gmail.com>
2022-10-28 16:03:03 -0700

    lkjmlkjmlkj

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,8 +2,9 @@
 #include "2-get_bit.c"
 /**
  * set_bit - sets a bit
- * @n: integer to grab 
- *
+ * @n: integer to grab
+ * @index: it is
+ * Return: int
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
