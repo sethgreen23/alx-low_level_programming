1eb7fa263d3dab1eb94de53d00dd9dc33e903677
sethgreen23 <dridi.chaith@gmail.com>
2022-10-31 12:54:59 -0700

    mlkjmlkj

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (1);
-	for (count = 0; text_content[count] != '\0', count++)
+	for (count = 0; text_content[count] != '\0'; count++)
 		;
 	wcount = write(fd, text_content, count);
 	if (wcount == -1)
