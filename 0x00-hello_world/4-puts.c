4aba7c2e6ebbbb8880562ba73320aa2989ab33fc
sethgreen23 <dridi.chaith@gmail.com>
2023-08-10 12:07:29 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function main
+ *
+ * Decription: print hello world
+ * Return: nothing
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
