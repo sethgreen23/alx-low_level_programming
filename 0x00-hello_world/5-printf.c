baab498e4a1e6a9d63759babe9a0903917a1f3b8
sethgreen23 <dridi.chaith@gmail.com>
2023-08-10 12:27:45 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function main
+ *
+ * Description: print a string
+ * Return: nothing
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+	return (0);
+}
