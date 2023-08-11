2bf780ef581b8189818fdc5a899f6e336433f184
sethgreen23 <dridi.chaith@gmail.com>
2023-08-11 11:53:35 -0700

    prints all decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: printing all digit numbers of
+ * base 10
+ * Return: Nothing
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		puts(i);
+	puts('\n');
+	return (0);
+}
