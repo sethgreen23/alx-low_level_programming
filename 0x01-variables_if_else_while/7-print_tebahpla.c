0d4dc3fc5dd83c3c23d952b5a0dc8e18f5025ecd
sethgreen23 <dridi.chaith@gmail.com>
2023-08-11 12:18:24 -0700

    print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Description: print alphabet in reverse
+ * Return: Nothing
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
