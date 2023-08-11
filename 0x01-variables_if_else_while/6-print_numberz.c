a0fdd1546b5d58c69821d853e96ba889117bcf0a
sethgreen23 <dridi.chaith@gmail.com>
2023-08-11 12:14:34 -0700

    print all decimal degit using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Decription: print all the decimal degits
+ * Return: Nothing
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
