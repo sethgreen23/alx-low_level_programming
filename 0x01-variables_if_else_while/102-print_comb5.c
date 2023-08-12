367a2abc985dc4226697eb3498c7593a6dc50dee
sethgreen23 <dridi.chaith@gmail.com>
2023-08-11 19:09:26 -0700

    comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: print smallest combination of
+ * three numbers
+ * Return: Nothing
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar(j + '0');
+				if (i == 98 && j == 99)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
