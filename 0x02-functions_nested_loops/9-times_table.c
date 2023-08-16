15d6ffed37a6b36a3c02efeb40cea59c5e194d9c
sethgreen23 <dridi.chaith@gmail.com>
2023-08-15 17:09:30 -0700

    print the times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - 9 times table
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i, j, result, element, decimal;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			result = i * j;
+			element = result % 10;
+			decimal = result / 10;
+			if (j == 9)
+			{
+				if (result < 10)
+				{
+					_putchar(j + '0');
+				}
+				else
+				{
+					_putchar(decimal + '0');
+					_putchar(element + '0');
+				}
+			}
+			else
+			{
+				print_space(result, decimal, element, i, j);
+			}
+		}
+		_putchar('\n');
+	}
+}
+/**
+ * print_space - print the space after the elements
+ * @result: the value
+ * @decimal: decimal value
+ * @element: element value
+ * @i: i value
+ * @j: j value
+ *
+ * Return: nothing
+ */
+void print_space(int result, int decimal, int element, int i, int j)
+{
+	if (result < 10)
+	{
+		_putchar(j + '0');
+	}
+	else
+	{
+		_putchar(decimal + '0');
+		_putchar(element + '0');
+	}
+	_putchar(' ');
+	result = i * (j + 1);
+	if (result > 9)
+	{
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(' ');
+	}
+}
