c84bd8d5134f4f619803fcab278005b09a3f20cd
sethgreen23 <dridi.chaith@gmail.com>
2023-08-15 17:12:19 -0700

    fix semicolne

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,5 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-void print_space(int result, int decimal, int element, int i, int j)
+void print_space(int result, int decimal, int element, int i, int j);
 #endif
