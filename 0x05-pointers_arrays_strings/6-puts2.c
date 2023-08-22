93e51055997aa51e6e64dbf0967053ff68678612
sethgreen23 <dridi.chaith@gmail.com>
2023-08-22 11:57:58 -0700

    put other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - print every other character
+ * @str: string
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+	char *p;
+
+	for (i = 0, p = str; *str != '\0'; i++, p++)
+	{
+		if (i % 2 == 0)
+			putchar(*p);
+	}
+}
