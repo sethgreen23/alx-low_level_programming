8b702170fc81a3b423e4639d44e52904427b2d38
sethgreen23 <dridi.chaith@gmail.com>
2023-08-23 16:30:31 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: value less, equal or less than 0 if s1 is found
+ */
+int _strcmp(char *s1, char *s2)
+{
+	char *s1_clone, *s2_clone;
+	int flag = 0;
+
+	s1_clone = s1;
+	s2_clone = s2;
+	for (; *s1_clone != '\0' && *s2_clone != '\0'; s1_clone++, s2_clone++)
+	{
+		if (*s1_clone > *s2_clone)
+		{
+			result = -1;
+			flag = 1;
+			break;
+		}
+		else if (*s1_clone < *s2_clone)
+		{
+			result = 1;
+			flag = 1;
+			break;
+		}
+	}
+	if (!flag)
+	{
+		if (*s1_clone == '\0' && *s2_clone == '\0')
+		{
+			result = 0;
+		}
+		else if (*s1_clone != '\0' && *s2_clone == '\0')
+		{
+			result = -1;
+		}
+		else if (*s1_clone == '\0' && *s2_clone != '\0')
+		{
+			result = 1;
+		}
+	}
+	return (result);
+}
