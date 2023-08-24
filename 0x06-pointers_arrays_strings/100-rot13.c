07339468d227310982ace076b1836ec90dccbbca
sethgreen23 <dridi.chaith@gmail.com>
2023-08-24 11:25:59 -0700

    root13 encoding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string to encode
+ *
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+	char *str_clone;
+
+	str_clone = str;
+	for (; *str_clone != '\0'; ++str_clone)
+	{
+		for (i = 0; input[i] != '\0'; ++i)
+		{
+			if (input[i] == *str_clone)
+				*str_clone = output[i];
+		}
+	}
+	return (str);
+
+}
