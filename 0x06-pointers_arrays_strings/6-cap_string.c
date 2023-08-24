02af49056dd974648c379cffba7e8b5f5ef38c15
sethgreen23 <dridi.chaith@gmail.com>
2023-08-24 04:57:54 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "main.h"
+#define SPACE ' '
+#define NEWLINE '\n'
+#define COMMA ','
+#define SEMICOLON ';'
+#define POINT '.'
+#define EXCLAMATION '!'
+#define INTEROGATION '?'
+#define QUOTATION '\"'
+#define OPENCURLLY '('
+#define CLOSECURLLY ')'
+#define OPENBRAKET '{'
+#define CLOSEBRAKET '}'
+#define ON 1
+#define OFF 0
+/**
+ * cap_string - capitalize all words of a string
+ * @str: string to operate on
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *str)
+{
+	int found;
+	char *str_clone;
+
+	found = OFF;
+	str_clone = str;
+	while (*str_clone != '\0')
+	{
+		if (*str_clone == SPACE ||
+			*str_clone == NEWLINE ||
+			*str_clone == COMMA ||
+			*str_clone == SEMICOLON ||
+			*str_clone == POINT ||
+			*str_clone == EXCLAMATION ||
+			*str_clone == INTEROGATION ||
+			*str_clone == QUOTATION ||
+			*str_clone == OPENCURLLY ||
+			*str_clone == CLOSECURLLY ||
+			*str_clone == OPENBRAKET ||
+			*str_clone == CLOSEBRAKET)
+		{
+			found = OFF;
+		}
+		else if (found == OFF)
+		{
+				if (*str_clone >= 97 && *str_clone <= 122)
+					*str_clone -= 32;
+				found = ON;
+		}
+		else
+			found = ON;
+		++str_clone;
+	}
+	return (str);
+}
