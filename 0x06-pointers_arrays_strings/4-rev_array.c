adb828ce08abd83a2c6f21ee924e759b5c4990ab
sethgreen23 <dridi.chaith@gmail.com>
2023-08-24 02:42:47 -0700

    reverse an array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,8 +13,8 @@ void reverse_array(int *a, int n)
 
 	for (i = 0, j = n - 1; i < j; i++, j++)
 	{
-		temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
+		temp = *(a + i);
+		*(a + i) = *(a + j);
+		*(a + j) = temp;
 	}
 }
