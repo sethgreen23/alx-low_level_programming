1e6590857360d8cb5cc9e22fbcf6bb53cffbc669
sethgreen23 <dridi.chaith@gmail.com>
2023-08-26 17:15:28 -0700

    infinit add 3

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
-void reverseStr(char *str)
+void reverseStr(char *str);
+void calc_two(char *s1, char *s2, char *r, int *i, int *kp, int *res, int s_r);
+void calc_one(char *s, char *r, int *i, int *kp, int *res, int s_r);
 #endif
