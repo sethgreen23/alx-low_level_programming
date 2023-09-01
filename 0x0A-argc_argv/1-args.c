eeab40ab37a7cbf452268c5340f1ad76b33ad507
sethgreen23 <dridi.chaith@gmail.com>
2023-09-01 11:25:16 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void argv);
+	printf("%d\n", argc);
+	return (0);
+}
