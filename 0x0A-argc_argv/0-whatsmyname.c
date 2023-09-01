0c47b8dff94586ec55e4d787ded04a32bdde4296
sethgreen23 <dridi.chaith@gmail.com>
2023-09-01 10:43:08 -0700

    another try

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main function
- * @argv: arguments array
- * @argc: number of elements in the array
+ * @argc: arguments array
+ * @argv: number of elements in the array
  *
- * Return: nothing
+ * Return: 0 on success 1 on error
  */
 int main(int argc, char *argv[])
 {
