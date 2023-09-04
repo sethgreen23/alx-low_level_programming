f8e684d9167874d22421c02bc40bce4bc1ed8550
sethgreen23 <dridi.chaith@gmail.com>
2023-09-04 08:16:17 -0700

    concatenation

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * str_concat - concatenates two days
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int size, i, j;
+	char *copy;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	copy = malloc(((strlen(s1) + strlen(s2)) * sizeof(char)) + 1);
+	if (copy == NULL)
+		exit(0);
+	for (i = 0; i < strlen(s1); i++)
+	{
+		copy[i] = s1[i];
+	}
+	for (j = 0; j < strlen(s2); i++, j++)
+	{
+		copy[i] = s2[j];
+	}
+	copy[i] = '\0';
+	return (copy);
+}
