62b326821682b16ade5746dec8ebae8e29125502
sethgreen23 <dridi.chaith@gmail.com>
2023-09-07 13:37:37 -0700

    strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * strtow - split a string into words
+ * @str: string
+ *
+ * Return: array of strings
+ */
+char **strtow(char *str)
+{
+	
+}
