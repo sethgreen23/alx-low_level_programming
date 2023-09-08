d49f3d3767bc67c6b08616990007e2f026d311ce
sethgreen23 <dridi.chaith@gmail.com>
2023-09-07 17:43:39 -0700

    conta 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,14 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size2, i, j, size1;
 	char *string;
 
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 	size1 = strlen(s1);
 	size2 = strlen(s2);
-	if (s2 == NULL || n == 0)
-		size2 = 0;
-	else if (n < strlen(s2))
+	if (n < strlen(s2))
 		size2 = n;
-	if (s1 == NULL)
-		size1 = 0;
 	string = (char *)malloc((size1 + size2 + 1) * sizeof(char));
 	if (string == NULL)
 		return (NULL);
