4b6aa2a3b25b02c8966c9be3fccf7a815ab3162b
sethgreen23 <dridi.chaith@gmail.com>
2023-09-11 13:15:25 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,26 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *nameCopy, *ownerCopy;
 
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	nameCopy = strdup(name);
+	if (nameCopy == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->name = nameCopy;
 	d->age = age;
-	d->owner = owner;
+	ownerCopy = strdup(owner);
+	if (ownercopy == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	d->owner = ownerCopy;
 	return (d);
 }
