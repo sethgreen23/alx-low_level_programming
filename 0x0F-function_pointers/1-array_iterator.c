bbaca4c4e6252cc72d5a2f9bf264ac513f5fbf25
sethgreen23 <dridi.chaith@gmail.com>
2023-09-13 05:35:49 -0700

    array iterator 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,5 +16,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL || size == 0)
 		return;
 	for (i = 0; i < size; i++)
-		array[i] = action(array[i];
+		array[i] = action(array[i]);
 }
