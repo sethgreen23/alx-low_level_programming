7a0cfc7b21f0e6c54369e0d0839e37d72b700cf0
sethgreen23 <dridi.chaith@gmail.com>
2023-09-13 12:09:10 -0700

    print bytes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * printBytes - print function bytes
+ * @func: function pointer
+ * @n: number of bytes prited
+ *
+ * Return: nothing
+ */
+void printBytes(void *func, int n)
+{
+	int i;
+	char *bytes;
+
+	bytes = (char *)func;
+	for (i = 0; i <n; i++)
+	{
+		printf("%.2hhx", bytes[i]);
+		if (i < n - 1 )
+			printf(" ");
+	}
+	printf("\n");
+}
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	printBytes(&main, n);
+	return (0);
+}
