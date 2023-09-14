9db98df4343f148cbdfa98bbf501f485b4f7e104
sethgreen23 <dridi.chaith@gmail.com>
2023-09-14 13:56:04 -0700

    good

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,33 +4,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * print - print and integer
- * @n: number to print
- *
- * Return:nothing
- */
-void print(int n)
-{
-	char num[32];
-	int i, j;
-
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	i = 0;
-	do {
-		num[i] = (n % 10) + '0';
-		n /= 10;
-		i++;
-	} while (n > 0);
-	for (j = i - 1; j >= 0; j--)
-	{
-		putchar(num[j]);
-	}
-}
-/**
  * print_numbers - print numbers followed by a new line
  * @separator: separator to use
  * @n: numbers of optional arguments
@@ -49,12 +22,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		current = va_arg(lst, int);
-		print(current);
+		printf("%d", current);
 		if (i < n - 1)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+			printf(", ");
 	}
 	va_end(lst);
 	putchar('\n');
