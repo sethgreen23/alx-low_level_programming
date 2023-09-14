caf56b74fcfb6a6faeaa530aa0bb19cf67716b2f
sethgreen23 <dridi.chaith@gmail.com>
2023-09-14 15:19:19 -0700

    print serparator not ,

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		current = va_arg(lst, int);
 		printf("%d", current);
 		if (i < n - 1)
-			printf(", ");
+			printf("%s ", separator);
 	}
 	va_end(lst);
 	putchar('\n');
