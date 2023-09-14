07ca7074fd9f2f138fd454b0c7ac70bf7b671334
sethgreen23 <dridi.chaith@gmail.com>
2023-09-14 15:29:46 -0700

    separator if not dont print it

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include "variadic_functions.h"
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * print_numbers - print numbers followed by a new line
  * @separator: separator to use
@@ -16,15 +15,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list lst;
 	int current;
 
-	if (separator == NULL)
-		return;
 	va_start(lst, n);
 	for (i = 0; i < n; i++)
 	{
 		current = va_arg(lst, int);
 		printf("%d", current);
 		if (i != n - 1)
-			printf("%s", separator);
+			printf("%s", separator == NULL ? '' : separator);
 	}
 	va_end(lst);
 	printf("\n");
