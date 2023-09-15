a50c67c881bd954e5d4f1aee2aff343b94a61c4c
sethgreen23 <dridi.chaith@gmail.com>
2023-09-14 18:22:30 -0700

    maybe 1

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
  * print_all - Prints all of the arguments when specified
  * @format: specifies the necessary operations
