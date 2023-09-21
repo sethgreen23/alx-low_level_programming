0694e42e09480242c6544e27589270053b0f9cd5
sethgreen23 <dridi.chaith@gmail.com>
2023-09-21 06:53:03 -0700

    1: length of the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * list_len - number of elements in a linked list
+ * @h: head of the list
+ *
+ * Return: Number of elements in the likedlist
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	const list_t *current;
+
+	if (head == NULL)
+		return (0);
+	current = h;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
