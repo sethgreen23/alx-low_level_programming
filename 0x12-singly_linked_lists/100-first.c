b23d815d10b25c9a5829477dc17744beb4cf7c63
sethgreen23 <dridi.chaith@gmail.com>
2023-09-21 10:26:12 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - function that execute before main
+ *
+ * Return: nothing
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
