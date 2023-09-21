8af6060ce216388c925644d6d65f073a16d0510c
sethgreen23 <dridi.chaith@gmail.com>
2023-09-21 10:07:01 -0700

    4: free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_list - free a list_t
+ * @head: head of the list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current = NULL;
+	list_t temp = NULL;
+
+	if (head == NULL)
+		return;
+	current = head;
+	while (current)
+	{
+		temp = current;
+		current = current->next;
+		free(str);
+		free(temp);
+	}
+}
