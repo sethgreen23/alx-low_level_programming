3b15483233d5ffc35cf4dccca3fc6f345e2d4655
sethgreen23 <dridi.chaith@gmail.com>
2023-09-21 10:45:24 -0700

    asymbly code that prints hello Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+	hello_message db "Hello, Holberton",10,0 ;
+
+section .text
+	global main
+
+extern printf
+
+main:
+	push rdi;
+	mov rdi, hello_message;
+	call printf;
+	pop rdi;
+
+	;
+	mov rax, 60;
+	xor rdi, rdi;
+	syscall
