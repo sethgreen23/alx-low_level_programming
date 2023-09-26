094154b3318c5ca677ba98afa4aa107b07c0ef21
sethgreen23 <dridi.chaith@gmail.com>
2023-09-26 10:20:55 -0700

    change again

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * len - length of the list
+ * @head: head of list
+ *
+ * Return: length of a list
+ */
+unsigned int len(listint_t *head)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		count++;
+		head = head->next;
+	}
+	return (count);
+}
+/**
  * get_nodeint_at_index - return the nth node of list_t
  * @head: head of the list
  * @index: index of the element
@@ -11,7 +30,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current = NULL;
 	unsigned int i = 0;
 
-	if (index > listint_len(head) || head == NULL)
+	if (index > len(head) || head == NULL)
 		return (NULL);
 	current = head;
 	while (i < index)
