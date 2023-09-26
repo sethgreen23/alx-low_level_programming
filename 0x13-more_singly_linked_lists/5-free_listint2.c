9ae99a7c8c7048f07559f7d5c60549f94094d08a
sethgreen23 <dridi.chaith@gmail.com>
2023-09-26 07:00:16 -0700

    change loginc 3

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = NULL;
 
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head->next);
+		free(*head);
+		*head = temp;
 	}
-	*head = NULL;
 }
