6e5fc55abc3174f514e42979da3e48bd43ea3589
sethgreen23 <dridi.chaith@gmail.com>
2023-09-26 07:05:58 -0700

    no

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp = NULL;
+	listint_t *current = *head;
 
-	while (*head != NULL)
+	while (*current != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = current->next;
+		free(current);
+		current = temp;
 	}
-	free(*head);
 }
