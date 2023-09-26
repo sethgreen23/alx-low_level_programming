5cf3b4c50df720702f61be6da7a2b7c2ab182f26
sethgreen23 <dridi.chaith@gmail.com>
2023-09-26 10:16:26 -0700

    get node int

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current = NULL;
 	unsigned int i = 0;
 
-	if (index > linsint_len(head) || head == NULL)
+	if (index > listint_len(head) || head == NULL)
 		return (NULL);
 	current = head;
 	while (i < index)
