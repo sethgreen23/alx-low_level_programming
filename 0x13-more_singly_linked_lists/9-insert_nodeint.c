6464cbb7414307320d2b30e351e9dfbc0343061d
sethgreen23 <dridi.chaith@gmail.com>
2023-09-26 13:24:05 -0700

    another try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,9 +9,12 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *current, *node;
+	unsigned int i;
+
 	if (head == NULL)
 		return (NULL);
-	if (idx < 0 || idx > len(*head))
+	if (idx > len(*head))
 		return (NULL);
 	node = (listint_t *)malloc(sizeof(listint_t));
 	if (node == NULL)
