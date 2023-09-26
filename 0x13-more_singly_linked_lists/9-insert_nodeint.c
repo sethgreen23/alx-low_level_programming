37976ed4f8fbec2df031690e621913d3c6a2a433
sethgreen23 <dridi.chaith@gmail.com>
2023-09-26 14:34:23 -0700

    lmjlkj

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = (listint_t *)malloc(sizeof(listint_t));
 	if (node == NULL)
 	{
-		free(node);
 		return (NULL);
 	}
 	node->n = n;
