e7a7ac7638fc60556cbf697f4cb6db72e7ace40c
sethgreen23 <dridi.chaith@gmail.com>
2023-09-27 10:44:26 -0700

    print safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,32 @@
 #include "lists.h"
 /**
+ * save_node - save the node in a array
+ * @list: the list containing the nodes
+ * @size: size of the list
+ * @head: is the node we will store
+ *
+ * Return: new list with the new node
+ */
+const listint_t **save_node(const listint_t **l, int s, const listint_t *h)
+{
+	int i = 0;
+	const listint_t **newList = NULL;
+
+	newList = (const listint_t **)malloc(sizeof(listint_t *) * s);
+	if (newList == NULL)
+	{
+		free(newList);
+		exit(98);
+	}
+	for (; i < s - 1; i++)
+	{
+		newList[i] = l[i];
+	}
+	newList[i] = h;
+	free(l);
+	return (newList);
+}
+/**
  * print_listint_safe - prints linked list
  * @head: head of the list
  *
@@ -7,14 +34,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current;
+	const listint_t **list = NULL;
+	int i = 0;
+	int size = 0;
 
-	if (head == NULL)
-		exit(98);
-	current = head;
-	while (current != NULL)
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", &current, current->n);
-		current = current->next;
+		for (i = 0; i < size; i++)
+		{
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (size);
+			}
+		}
+		size++;
+		list = save_node(list, size, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
+	free(list);
+	return (size);
 }
