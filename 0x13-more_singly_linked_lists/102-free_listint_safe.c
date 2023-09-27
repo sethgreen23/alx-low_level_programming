bde7fd4d3a58732f301721b8a42737e5db064772
sethgreen23 <dridi.chaith@gmail.com>
2023-09-27 13:32:34 -0700

    safe free

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+/**
+ * save_n - save node
+ * @l: list
+ * @s: size
+ * @h: header node
+ *
+ * Return: new list with the nodes
+ */
+listint_t **save_n(listint_t **l, size_t s, listint_t *h)
+{
+	listint_t **newList = NULL;
+	size_t i;
+
+	newList = malloc(sizeof(listint_t *));
+	if (newList == NULL)
+	{
+		free(l);
+		free(newList);
+		exit(98);
+	}
+	for (i = 0; i < s - 1; i++)
+	{
+		newList[i] = l[i];
+	}
+	newList[i] = h;
+	free(l);
+	return (newList);
+}
+
+/**
+ * free_listint_safe - frees list
+ * @h: head of the list
+ *
+ * Return: size of the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t **list = NULL;
+	size_t size = 0, i = 0;
+	listint_t *temp = NULL;
+
+	if (h == NULL || *h == NULL)
+		return (size);
+	while (*h != NULL)
+	{
+		if (*h == list[i])
+		{
+			*h = NULL;
+			free(list);
+			return (size);
+		}
+		size++;
+		list = save_n(list, size, *h);
+		temp = (*h)->next;
+		free(*h);
+		*h = temp;
+	}
+	free(list);
+	return (size);
+}
