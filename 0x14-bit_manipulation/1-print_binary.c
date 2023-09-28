a136c6e37c63b06738c7a753523f18af9c96fb38
sethgreen23 <dridi.chaith@gmail.com>
2023-09-28 03:48:48 -0700

    change logic

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,23 +7,28 @@
  */
 void print_binary(unsigned long int n)
 {
-	int flag = 0, i;
+	int flag = 0, bit = 31;
+	unsigned long int i;
 
 	if (n == 0)
 	{
 		_putchar('0');
+		return;
 	}
-	for (i = 1 << 31; i > 0; i = i / 2)
+	for (i = 1 << bit; bit >= 0; i = 1 << bit)
 	{
 		if (flag)
 		{
-			(n & i) ? _putchar('1') : putchar('0');		
+			(n & i) ? _putchar('1') : _putchar('0');		
 		}
 		else
 		{
 			if (n & i)
+			{
 				_putchar('1');
-			flag = 1;
+				flag = 1;
+			}
 		}
+		--bit;
 	}
 }
