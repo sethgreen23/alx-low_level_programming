0e2a0c2a39976742f0c77049613fb15e7c3bbbdb
sethgreen23 <dridi.chaith@gmail.com>
2023-09-28 03:29:07 -0700

    change 3

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,26 +7,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bits = 31;
-	int flag = 0;
+	int flag = 0, i;
 
 	if (n == 0)
 	{
 		_putchar('0');
 	}
-	for (; bits >= 0; bits--)
+	for (i = 1 << 31; i > 0; i = i / 2)
 	{
-		if (flag == 0)
+		if (flag)
 		{
-			if ((n >> bits) & 1)
-			{
-				_putchar(((n >> bits) & 1) + '0');
-				flag = 1;
-			}
+			(n & i) ? _putchar('1') : putchar('0');		
 		}
 		else
 		{
-			_putchar(((n >> bits) & 1) + '0');
+			if (n & i)
+				_putchar('1');
+			flag = 1;
 		}
 	}
 }
