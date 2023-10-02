b29650f378ff60a8723d8a6adf4052541cc86112
sethgreen23 <dridi.chaith@gmail.com>
2023-10-02 12:47:14 -0700

    copy the files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t o, size;
+	ssize_t o, size, i;
 	char *buffer;
 
 	if (filename == NULL)
@@ -25,14 +25,15 @@ int create_file(const char *filename, char *text_content)
 	size = 0;
 	while (text_content[size] != '\0')
 		size++;
-	buffer = malloc(sizeof(char) * (size + 1));
+	buffer = malloc(sizeof(char) * size);
 	if (buffer == NULL)
 	{
 		close(o);
 		return (-1);
 	}
-
-	if (write(o, buffer, sizeof(buffer) - 1) == -1)
+	for (i = 0; i < size; i++)
+		buffer[i] = text_content[i];
+	if (write(o, buffer, sizeof(buffer)) == -1)
 	{
 		free(buffer);
 		close(o);
