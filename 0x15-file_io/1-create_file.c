a432fb03bd7a01c2d7f5335de0821aa34f0e4849
sethgreen23 <dridi.chaith@gmail.com>
2023-10-02 12:18:14 -0700

    hello

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[size] != '\0')
 		size++;
 
-	buffer = malloc(sizeof(char) * size);
+	buffer = malloc(sizeof(char) * (size + 1));
 	if (buffer == NULL)
 	{
 		close(o);
