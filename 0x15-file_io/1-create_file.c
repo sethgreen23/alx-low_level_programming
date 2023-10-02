d3c05cfccc1a1f4d0c0eba22c01c76c7e30e394b
sethgreen23 <dridi.chaith@gmail.com>
2023-10-02 12:14:12 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * create_file - creates file
+ * @filename: filename
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, size;
+	char *buffer;
+
+	if (filename == NULL)
+		return (-1);
+	o = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (text_content == NULL)
+	{
+		close(o);
+		return (1);
+	}
+
+	size = 0;
+	while (text_content[size] != '\0')
+		size++;
+
+	buffer = malloc(sizeof(char) * size);
+	if (buffer == NULL)
+	{
+		close(o);
+		return (-1);
+	}
+
+	if (write(o, buffer, size) <= 0)
+	{
+		free(buffer);
+		close(o);
+		return (-1);
+	}
+	free(buffer);
+	close(o);
+	return (1);
+
+}
