68cd11934ccddfd70cef9b4221a5f3d90069b064
sethgreen23 <dridi.chaith@gmail.com>
2023-10-02 11:36:07 -0700

    changing logic

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,27 +9,24 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file_output = 0;
-	ssize_t bytes_read = 0;
-	ssize_t bytes_written = 0;
+	ssize_t file_output, bytes_read, bytes_written;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		free(buffer);
 		return (0);
-	}
+
 	file_output = open(filename, O_RDONLY);
 	bytes_read = read(file_output, buffer, letters);
-	bytes_written = write(1, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	if (file_output == -1
-		|| bytes_read == -1
-		|| bytes_written == -1
-		|| bytes_read != bytes_written)
+			|| bytes_read == -1
+			|| bytes_written == -1
+			|| bytes_read != bytes_written)
 	{
 		free(buffer);
 		return (0);
