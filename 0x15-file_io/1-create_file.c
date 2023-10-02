04d5c6ae9f560a6931a0b92c117eeacf0149fa19
sethgreen23 <dridi.chaith@gmail.com>
2023-10-02 13:02:03 -0700

    try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,39 +8,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t o, size, i;
-	char *buffer;
+	int o, w, i;
 
 	if (filename == NULL)
 		return (-1);
-	o = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-	if (o == -1)
-		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(o);
-		return (1);
+		for (i = 0; text_content[i] != '\0'; i++)
+			size++;
 	}
-
-	size = 0;
-	while (text_content[size] != '\0')
-		size++;
-	buffer = malloc(sizeof(char) * size);
-	if (buffer == NULL)
-	{
-		close(o);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, size);
+	if (o == -1 || w == -1)
 		return (-1);
-	}
-	for (i = 0; i < size; i++)
-		buffer[i] = text_content[i];
-	if (write(o, buffer, sizeof(buffer)) == -1)
-	{
-		free(buffer);
-		close(o);
-		return (-1);
-	}
-	free(buffer);
 	close(o);
 	return (1);
-
 }
