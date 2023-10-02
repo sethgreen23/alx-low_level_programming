56f28776d61a80fd3823e420a095a8f285782fb7
sethgreen23 <dridi.chaith@gmail.com>
2023-10-02 12:53:25 -0700

    focus

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	o = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	o = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (o == -1)
 		return (-1);
 	if (text_content == NULL)
