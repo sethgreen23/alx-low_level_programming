0d6724abd7d0b9b8f7d58025c637c66a4cd18b36
sethgreen23 <dridi.chaith@gmail.com>
2023-11-09 07:38:11 -0800

    task0: change logic

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (0);
-	size = size_dlistint(h);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		size++;
 	}
 	return (size);
 }
