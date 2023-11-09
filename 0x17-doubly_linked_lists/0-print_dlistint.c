c0e061469def42be95696bf12f8bc1e5ec50ef4c
sethgreen23 <dridi.chaith@gmail.com>
2023-11-09 07:14:44 -0800

    task0: print doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "list.h"
+/**
+ * print_dlistint - print all elements of a doubly linked list
+ * @h: head
+ *
+ * Return: size of the doubly likded list of 0 if empty
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	size = size_dlistint(h)
+	if (h == NULL)
+		return (size);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (size);
+}
---
+++ 0x17-doubly_linked_lists/size_dlistint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+/**
+ * size_dlistint - length of the doubly linked list
+ * @h: head
+ *
+ * Return: size of doubly linked list or 0 if empty
+ */
+size_t size_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		size++;
+		h = h->next;
+	}
+	return (size);
+}
