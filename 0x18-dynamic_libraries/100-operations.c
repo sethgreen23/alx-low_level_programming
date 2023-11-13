6d3159c6bfbaac94a117a87637c082ad82a2acd3
sethgreen23 <dridi.chaith@gmail.com>
2023-11-13 04:04:58 -0800

    task3

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * add - add to values
+ * @a: a
+ * @b: b
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub of  two values
+ * @a: a
+ * @b: b
+ *
+ * Return: sub of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mul of  two values
+ * @a: a
+ * @b: b
+ *
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - mod of  two values
+ * @a: a
+ * @b: b
+ *
+ * Return: mod of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
