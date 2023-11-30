42f9997c4d60ad3a8769c329cdc80ea76a50f24c
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 15:29:04 -0800

    fixing bugs

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,36 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - create a node
+ * @key: key
+ * @value: value
+ *
+ * Return: node or NULL
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *hn = NULL;
+
+	hn = malloc(sizeof(hash_node_t));
+	if (hn == NULL)
+		return (NULL);
+	hn->key = strdup(key);
+	if (hn->key == NULL)
+	{
+		free(hn), hn = NULL;
+		return (NULL);
+	}
+	hn->value = strdup(value);
+	if (hn->value == NULL)
+	{
+		free(hn->key), hn->key = NULL;
+		free(hn), hn = NULL;
+		return (NULL);
+	}
+	hn->next = NULL;
+	return (hn);
+}
+/**
  * hash_table_set - function that adds element to the hash table
  *
  * @ht: hash table
@@ -11,41 +41,30 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *hn = NULL;
+	hash_node_t *hn = NULL, *tmp = NULL;
+	char *new_value = NULL;
 	unsigned long int bucketIndex;
 
 	if (ht == NULL || strlen(key) == 0 || key == NULL || value == NULL ||
 	    ht->size == 0 || ht->array == NULL)
 		return (0);
 
-	hn = malloc(sizeof(hash_node_t));
-	if (hn == NULL)
-		return (0);
+	bucketInde = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[bucketIndex];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			tmp->value = new_value;
+		}
+		tmp = tmp->next;
+	}
+	hn = create_node(key, value);
+	hn->next = ht->array[bucketIndex];
+	ht->array[bucketIndex] = hn;
 
-	hn->key = strdup(key);
-	if (hn->key == NULL)
-	{
-		free(hn), hn = NULL;
-		return (0);
-	}
-	hn->value = strdup(value);
-	if (hn->value == NULL)
-	{
-		free(hn->key), hn->key = NULL;
-		free(hn), hn = NULL;
-		return (0);
-	}
-	hn->next = NULL;
-
-	bucketIndex = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[bucketIndex] == NULL)
-	{
-		ht->array[bucketIndex] = hn;
-	}
-	else
-	{
-		hn->next = ht->array[bucketIndex];
-		ht->array[bucketIndex] = hn;
-	}
 	return (1);
 }
