d582dca959f2d66f3cb42bb897111f1e3d43b40c
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 12:56:58 -0800

    python programming

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	hn->value = strdup(value);
-	if (hn->value == NULL);
+	if (hn->value == NULL)
 	{
 		free(hn->key);
 		free(hn);
