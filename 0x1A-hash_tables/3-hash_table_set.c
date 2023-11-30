8b762b13eb08a2e5502c828c468ab3cd14dece6b
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 13:34:37 -0800

    kjlk

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hn = NULL;
 	unsigned long int bucketIndex;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL)
+		return (0);
+	if (key == NULL)
 		return (0);
 
 	hn = malloc(sizeof(hash_node_t));
