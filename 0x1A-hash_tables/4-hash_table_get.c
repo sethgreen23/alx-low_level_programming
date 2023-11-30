2aabc88b0e4abf69a04e0931babdab5cfc41ad02
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 15:48:21 -0800

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp = NULL;
+	unsigned long int bucketIndex = 0;
+
+	if (ht == NULL || ht->size == 0 || ht->array == NULL ||
+	    strlen(key) == 0 || key == NULL)
+		return (NULL);
+	bucketIndex = key_index(key, ht->size);
+	tmp = ht->array[bucketIndex];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
