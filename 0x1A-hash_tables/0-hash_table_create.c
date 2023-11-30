979f9452480d636f9d05188aa133f07d4415da51
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 07:04:00 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hashtable
+ *
+ * @size: The size of the hashtable
+ *
+ * Return: a hashtable
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * ht->size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+	}
+	return (ht);
+}
