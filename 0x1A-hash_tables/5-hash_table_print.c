688ffc577d9f03751a39246d8072a7ee79fcb6b6
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 16:45:22 -0800

    5taks

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hashtable
+ *
+ * @ht: hashtable
+ *
+ * Return: Nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	int flag = 0;
+
+	if (ht == NULL || ht->size == 0 || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->arra[i];
+		while (tmp != NULL)
+		{
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (tmp->next != NULL)
+				printf(", ");
+			tmp = tmp->next;
+			flag = 1;
+		}
+		if (ht->array[i + 1] != NULL && flag)
+			printf(", ");
+	}
+	printf("}\n");
+}
