395d2a92381ce461d0e82f6858701f3065431d58
sethgreen23 <dridi.chaith@gmail.com>
2023-11-30 17:38:31 -0800

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table
+ *
+ * REturn: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp = NULL, *current = NULL;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			tmp = current;
+			current = current->next;
+			free(tmp->value), tmp->value = NULL;
+			free(tmp->key), tmp->key = NULL;
+			tmp->next = NULL;
+			free(tmp);
+		}
+	}
+	free(ht);
+}
