c881982bf988ed7203416f00b471f9e02a082bd3
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-12 20:28:05 +0100

    task 105

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,66 @@
+#include "search_algos.h"
+#include <unistd.h>
+
+
+/**
+ * jump_list - implementation of jump search
+ *
+ * @list: the singly linked list
+ * @size: size of the array
+ * @value: value to search
+ *
+ * Return: index otherwise NULL
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t end, start, i;
+	int start_v, value_i;
+
+	if (!list)
+		return (NULL);
+	start = 0;
+	end = sqrt(size);
+	while (end < size && (jump(list, size, end))->n < value)
+	{
+		start_v = (jump(list, size, start))->n;
+		printf("Value checked at index [%ld] = [%d]\n", start, start_v);
+		start = end;
+		end = start + sqrt(size);
+	}
+	printf("Value checked at index [%ld] = [%d]\n", start, start_v);
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+	if (end >= size)
+		end = size - 1;
+	for (i = start; i <= end; i++)
+	{
+		value_i = (jump(list, size, i))->n;
+		printf("Value checked at index[%ld] = [%d]\n", i, value_i);
+		if (value_i == value)
+			return (jump(list, size, i));
+	}
+	return (NULL);
+}
+
+/**
+ * jump - go to sertain index in list
+ *
+ * @list: the singly linked list
+ * @size: size of the array
+ * @index: index to go to
+ *
+ * Return: index otherwise NULL
+ */
+listint_t *jump(listint_t *list, size_t size, size_t index)
+{
+	listint_t *p;
+
+	p = list;
+	if (index > size)
+		return (NULL);
+	while (index > 0)
+	{
+		p = p->next;
+		index--;
+	}
+	return (p);
+}
