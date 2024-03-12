9724931a64c2c7aea39e072eda1e6477b296bdfb
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-12 12:00:17 +0100

    task 102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,7 +30,7 @@ int interpolation_search(int *array, size_t size, int value)
 			low = probe + 1;
 	}
 	probe = low + (((double)(high - low) / (array[high] - array[low])) *
-                                (value - array[low]));
+			(value - array[low]));
 	printf("Value checked array[%ld] is out of range\n", probe);
 	return (-1);
 }
